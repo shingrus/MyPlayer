dba19fc8a3de7f0a1c6e1ce2f9bacff188775187
shingrus <shingrus@gmail.com>
2011-08-02 01:27:38 +0400

    initial commit for Application src

---
+++ src/com/shingrus/MyPlayerApplication.java
@@ -0,0 +1,15 @@
+package com.shingrus;
+
+import android.app.Application;
+import java.lang.*;
+import java.util.*;
+
+public class MyPlayerApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		//TODO: here we need to start new thread class
+		super.onCreate();
+	}
+
+}
