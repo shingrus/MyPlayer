e6269a68bd69dc974b6cb312ce632c8167458f36
shingrus <shingrus@gmail.com>
2011-08-02 10:41:37 +0400

    initial commit for UpdateService

---
+++ src/com/shingrus/UpdateService.java
@@ -0,0 +1,27 @@
+package com.shingrus;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class UpdateService extends Service {
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
